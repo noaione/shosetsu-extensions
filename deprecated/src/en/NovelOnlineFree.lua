122f86a76b6c922e13af753b7af78df47a776658
noaione <noaione0809@gmail.com>
2023-01-03 14:35:37 +0700

    chore: cleanup deprecated folder

--- deprecated/src/en/NovelOnlineFree.lua
+++
@@ -1,7 +0,0 @@
--- {"id":814,"version":"1.0.0","author":"Doomsdayrs"}
--- Direct ripooff of BestLightNovel
-return Require("247truyen")("https://novelonlinefree.com", {
-	id = 814,
-	name = "NovelOnlineFree",
-	shrinkURLNovel = "novel",
-})
--- deprecated/src/en/NovelOnlineFull.lua
+++
@@ -1,8 +0,0 @@
--- {"id":510,"version":"1.0.0","author":"Doomsdayrs"}
--- Direct ripooff of BestLightNovel
-return Require("247truyen")("https://novelonlinefull.com", {
-	id = 510,
-	name = "NovelOnlineFull",
-	novelListingTitleClass = "",
-	shrinkURLNovel = "novel",
-})
