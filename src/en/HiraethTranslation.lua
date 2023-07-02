2788c08d31e112733a725da73160036d03a89933
noaione <noaione0809@gmail.com>
2023-07-02 15:21:07 +0000

    📈 Automatic deployment from noaione/shosetsu-extensions@a2f79f697821b28f2653d1d4ebe7507e9618ddad

---
+++ src/en/HiraethTranslation.lua
@@ -0,0 +1,35 @@
+-- {"id":43148,"ver":"0.1.0","libVer":"1.0.0","author":"N4O","dep":["Madara>=2.9.0"]}
+
+return Require("Madara")("https://hiraethtranslation.com", {
+    id = 43148,
+    name = "Hiraeth Translations",
+    imageURL = "https://github.com/noaione/shosetsu-extensions/raw/dev/icons/Hiraeth.png",
+
+    -- defaults values
+    -- latestNovelSel = "div.page-listing-item",
+    ajaxUsesFormData = true,
+
+    -- There are paid chapters, we can ignore it
+    chaptersListSelector = "li.wp-manga-chapter.free-chap",
+
+    genres = {
+        "Action",
+        -- "Adventure",
+        "Comedy",
+        -- "Dark Elf",
+        -- "Drama",
+        "Fantasy",
+        "Harem",
+        -- "Isekai",
+        -- "Mecha",
+        -- "Mystery",
+        -- "Original Works",
+        "Psychological",
+        -- "Rom-Com",
+        "Romance",
+        "Schoo Lifel",
+        -- "Shoujo",
+        -- "Slice of Life",
+        "Seinen",
+    }
+})
