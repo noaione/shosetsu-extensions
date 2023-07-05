dfca8d791d8ae6c0ad7ee60df3bbea5bb817e365
noaione <noaione0809@gmail.com>
2023-07-05 12:36:09 +0000

    📈 Automatic deployment from noaione/shosetsu-extensions@61bfec24fe59a8ac7f7b44dd8e6224a1883124a8

--- src/en/HiraethTranslation.lua
+++ src/en/HiraethTranslation.lua
@@ -1,4 +1,4 @@
--- {"id":43148,"ver":"0.1.0","libVer":"1.0.0","author":"N4O","dep":["Madara>=2.9.0"]}
+-- {"id":43148,"ver":"0.1.1","libVer":"1.0.0","author":"N4O","dep":["Madara>=2.9.0"]}
 
 return Require("Madara")("https://hiraethtranslation.com", {
     id = 43148,
@@ -6,8 +6,8 @@ return Require("Madara")("https://hiraethtranslation.com", {
     imageURL = "https://github.com/noaione/shosetsu-extensions/raw/dev/icons/Hiraeth.png",
 
     -- defaults values
-    -- latestNovelSel = "div.page-listing-item",
-    ajaxUsesFormData = true,
+    latestNovelSel = "div.page-listing-item",
+    ajaxUsesFormData = false,
 
     -- There are paid chapters, we can ignore it
     chaptersListSelector = "li.wp-manga-chapter.free-chap",
