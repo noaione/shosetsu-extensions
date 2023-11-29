8995f9f8379afe86a2d6f58cdf921384edfb4eb2
noaione <noaione0809@gmail.com>
2023-11-29 08:37:33 +0000

    📈 Automatic deployment from noaione/shosetsu-extensions@ab470cd63a021373465768efe6c8f826edb4740c

--- src/en/Awebstories.lua
+++ src/en/Awebstories.lua
@@ -1,4 +1,4 @@
--- {"id":954054,"ver":"0.1.0","libVer":"1.0.0","author":"N4O","dep":["Bixbox>=1.0.0","WPCommon>=1.0.2"]}
+-- {"id":954054,"ver":"0.2.0","libVer":"1.0.0","author":"N4O","dep":["Bixbox>=1.1.0","WPCommon>=1.0.2"]}
 
 local WPCommon = Require("WPCommon")
 
@@ -6,6 +6,35 @@ return Require("Bixbox")("https://awebstories.com", {
     id = 954054,
     name = "Awebstories",
     imageURL = "https://github.com/noaione/shosetsu-extensions/raw/dev/icons/Awebstories.png",
+
+    availableGenres = {
+        "Action",
+        "Adventure",
+        "Comedy",
+        "Drama",
+        "Ecchi",
+        "Fantasy",
+        "Harem",
+        "Historical",
+        "Horror",
+        "Martial Arts",
+        "Mature",
+        "Mystery",
+        "Psychological",
+        "Romance",
+        "School Life",
+        "Seinen",
+        "Shoujo",
+        "Shounen",
+        "Slice of Life",
+        "Supernatural",
+        "Tragedy"
+    },
+
+    availableTypes = {
+        "Published Novel (JP)",
+        "Web Novel"
+    },
 
     --- @param content Element
     stripMechanics = function (content)
