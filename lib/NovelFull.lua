0f1f657c24e555fc5da5c764aeca7d49da725550
noaione <noaione0809@gmail.com>
2024-05-17 02:17:53 +0000

    📈 Automatic deployment from noaione/shosetsu-extensions@d8145a265237e5d4a1b692d5603e71aa252cde8c

--- lib/NovelFull.lua
+++ lib/NovelFull.lua
@@ -6,133 +6,133 @@
 local qs = Require("url").querystring
 
 local text = function(v)
-	return v:text()
+    return v:text()
 end
 
 local defaults = {
-	meta_offset = 1,
-	ajax_hot = "/ajax/hot-novels",
-	ajax_latest = "/ajax/latest-novels",
-	ajax_chapters = "/ajax/chapter-option",
-	appendURLToInfoImage = true,
-	searchTitleSel = ".novel-title",
+    meta_offset = 1,
+    ajax_hot = "/ajax/hot-novels",
+    ajax_latest = "/ajax/latest-novels",
+    ajax_chapters = "/ajax/chapter-option",
+    appendURLToInfoImage = true,
+    searchTitleSel = ".novel-title",
 
-	hasCloudFlare = false,
-	hasSearch = true,
-	chapterType = ChapterType.HTML
+    hasCloudFlare = false,
+    hasSearch = true,
+    chapterType = ChapterType.HTML
 }
 
 function defaults:search(data)
-	-- search gives covers but they're in some weird aspect ratio
-	local doc = GETDocument(qs({ keyword = data[QUERY], page = data[PAGE] }, self.baseURL .. "/search"))
+    -- search gives covers but they're in some weird aspect ratio
+    local doc = GETDocument(qs({ keyword = data[QUERY], page = data[PAGE] }, self.baseURL .. "/search"))
 
-	return map(doc:selectFirst("div." .. self.searchListSel):select("div.row"), function(v)
-			local novel = Novel()
-			novel:setImageURL(v:selectFirst("img.cover"):attr("src"))
-			local d = v:selectFirst(self.searchTitleSel .. " a")
-			novel:setLink(d:attr("href"))
-			novel:setTitle(d:attr("title"))
-			return novel
-		end)
+    return map(doc:selectFirst("div." .. self.searchListSel):select("div.row"), function(v)
+            local novel = Novel()
+            novel:setImageURL(v:selectFirst("img.cover"):attr("src"))
+            local d = v:selectFirst(self.searchTitleSel .. " a")
+            novel:setLink(d:attr("href"))
+            novel:setTitle(d:attr("title"))
+            return novel
+        end)
 end
 
 function defaults:getPassage(url)
-	local htmlElement = GETDocument(self.baseURL..url):selectFirst("div#chapter")
-	local title = htmlElement:selectFirst("a.chapter-title"):text()
-	htmlElement = htmlElement:selectFirst("div#chapter-content")
+    local htmlElement = GETDocument(self.baseURL..url):selectFirst("div#chapter")
+    local title = htmlElement:selectFirst("a.chapter-title"):text()
+    htmlElement = htmlElement:selectFirst("div#chapter-content")
 
-	-- Remove/modify unwanted HTML elements to get a clean webpage.
-	htmlElement:removeAttr("style") -- Hopefully only temporary as a hotfix
-	htmlElement:select("script"):remove()
-	htmlElement:select("ins"):remove()
-	htmlElement:select("div.ads"):remove()
-	htmlElement:select("div[align=\"left\"]:last-child"):remove() -- Report error text
+    -- Remove/modify unwanted HTML elements to get a clean webpage.
+    htmlElement:removeAttr("style") -- Hopefully only temporary as a hotfix
+    htmlElement:select("script"):remove()
+    htmlElement:select("ins"):remove()
+    htmlElement:select("div.ads"):remove()
+    htmlElement:select("div[align=\"left\"]:last-child"):remove() -- Report error text
 
-	-- Chapter title inserted before chapter text.
-	htmlElement:child(0):before("<h1>" .. title .. "</h1>");
+    -- Chapter title inserted before chapter text.
+    htmlElement:child(0):before("<h1>" .. title .. "</h1>");
 
-	return pageOfElem(htmlElement)
+    return pageOfElem(htmlElement)
 end
 
 function defaults:parseNovel(url, loadChapters)
-	local doc = GETDocument(self.baseURL..url)
-	local info = NovelInfo()
+    local doc = GETDocument(self.baseURL..url)
+    local info = NovelInfo()
 
-	local elem = doc:selectFirst(".info"):children()
-	info:setTitle(doc:selectFirst("h3.title"):text())
+    local elem = doc:selectFirst(".info"):children()
+    info:setTitle(doc:selectFirst("h3.title"):text())
 
-	local meta_offset = elem:size() < 3 and self.meta_offset or 0
+    local meta_offset = elem:size() < 3 and self.meta_offset or 0
 
-	local function meta_links(i)
-		return map(elem:get(meta_offset + i):select("a"), text)
-	end
+    local function meta_links(i)
+        return map(elem:get(meta_offset + i):select("a"), text)
+    end
 
-	info:setAuthors(meta_links(0))
-	info:setAlternativeTitles(meta_links(1))
-	info:setGenres(meta_links(2))
-	info:setStatus( ({
-		Ongoing = NovelStatus.PUBLISHING,
-		Completed = NovelStatus.COMPLETED
-	})[elem:get(meta_offset + 4):select("a"):text()] )
+    info:setAuthors(meta_links(0))
+    info:setAlternativeTitles(meta_links(1))
+    info:setGenres(meta_links(2))
+    info:setStatus( ({
+        Ongoing = NovelStatus.PUBLISHING,
+        Completed = NovelStatus.COMPLETED
+    })[elem:get(meta_offset + 4):select("a"):text()] )
 
-	info:setImageURL((self.appendURLToInfoImage and self.baseURL or "") .. doc:selectFirst("div.book img"):attr("src"))
-	info:setDescription(table.concat(map(doc:select("div.desc-text p"), text), "\n"))
+    info:setImageURL((self.appendURLToInfoImage and self.baseURL or "") .. doc:selectFirst("div.book img"):attr("src"))
+    info:setDescription(table.concat(map(doc:select("div.desc-text p"), text), "\n"))
 
-	if loadChapters then
-		local id = doc:selectFirst("div[data-novel-id]"):attr("data-novel-id")
-		local i = 0
-		info:setChapters(AsList(map(
-				GETDocument(qs({ novelId = id,currentChapterId = "" }, self.ajax_base .. self.ajax_chapters)):selectFirst("select"):children(),
-				function(v)
-					local chap = NovelChapter()
-					chap:setLink(self.shrinkURL(v:attr("value")))
-					chap:setTitle(v:text())
-					chap:setOrder(i)
-					i = i + 1
-					return chap
-				end)))
-	end
+    if loadChapters then
+        local id = doc:selectFirst("div[data-novel-id]"):attr("data-novel-id")
+        local i = 0
+        info:setChapters(AsList(map(
+                GETDocument(qs({ novelId = id,currentChapterId = "" }, self.ajax_base .. self.ajax_chapters)):selectFirst("select"):children(),
+                function(v)
+                    local chap = NovelChapter()
+                    chap:setLink(self.shrinkURL(v:attr("value")))
+                    chap:setTitle(v:text())
+                    chap:setOrder(i)
+                    i = i + 1
+                    return chap
+                end)))
+    end
 
-	return info
+    return info
 end
 
 ---@param url string
 function defaults:shrinkURL(url)
-	return url:gsub(self.baseURL, "")
+    return url:gsub(self.baseURL, "")
 end
 
 ---@param url string
 function defaults:expandURL(url)
-	return self.baseURL .. url
+    return self.baseURL .. url
 end
 
 return function(baseURL, _self)
-	_self = setmetatable(_self or {}, { __index = function(_, k)
-		local d = defaults[k]
-		return (type(d) == "function" and wrap(_self, d) or d)
-	end })
-	_self["baseURL"] = baseURL
-	if not _self["ajax_base"] then
-		_self["ajax_base"] = baseURL
-	end
-	_self["listings"] = {
-		Listing("Hot", false, function()
-			return map(GETDocument(_self.ajax_base .. _self.ajax_hot):select("div.item a"), function(v)
-				local novel = Novel()
-				novel:setImageURL(baseURL .. v:selectFirst("img"):attr("src"))
-				novel:setTitle(v:attr("title"))
-				novel:setLink(v:attr("href"))
-				return novel
-			end)
-		end),
-		Listing("Latest", false, function()
-			return map(GETDocument(_self.ajax_base .. _self.ajax_latest):select("div.row .col-title a"), function(v)
-				local novel = Novel()
-				novel:setTitle(v:text())
-				novel:setLink(v:attr("href"))
-				return novel
-			end)
-		end)
-	}
-	return _self
+    _self = setmetatable(_self or {}, { __index = function(_, k)
+        local d = defaults[k]
+        return (type(d) == "function" and wrap(_self, d) or d)
+    end })
+    _self["baseURL"] = baseURL
+    if not _self["ajax_base"] then
+        _self["ajax_base"] = baseURL
+    end
+    _self["listings"] = {
+        Listing("Hot", false, function()
+            return map(GETDocument(_self.ajax_base .. _self.ajax_hot):select("div.item a"), function(v)
+                local novel = Novel()
+                novel:setImageURL(baseURL .. v:selectFirst("img"):attr("src"))
+                novel:setTitle(v:attr("title"))
+                novel:setLink(v:attr("href"))
+                return novel
+            end)
+        end),
+        Listing("Latest", false, function()
+            return map(GETDocument(_self.ajax_base .. _self.ajax_latest):select("div.row .col-title a"), function(v)
+                local novel = Novel()
+                novel:setTitle(v:text())
+                novel:setLink(v:attr("href"))
+                return novel
+            end)
+        end)
+    }
+    return _self
 end
--- src/en/StorySeedling.lua
+++ src/en/StorySeedling.lua
@@ -30,285 +30,285 @@ local GENRE_FILTER = 50
 -- and Selecting "Show in Console"
 -- This is just a quick and dirty way to quickly update the genres.
 local GENRE_VALUES = { 
-	"Action: 111",
-	"Adult: 183",
-	"Adventure: 112",
-	"BL: 207",
-	"Comedy: 153",
-	"Drama: 115",
-	"Ecchi: 170",
-	"Fantasy: 114",
-	"Harem: 956",
-	"Historical: 178",
-	"Horror: 254",
-	"Josei: 472",
-	"Martial Arts: 1329",
-	"Mature: 427",
-	"Mecha: 1481",
-	"Mystery: 645",
-	"Psychological: 515",
-	"Reincarnation: 1031",
-	"Romance: 108",
-	"School Life: 545",
-	"Sci-Fi: 113",
-	"Seinen: 708",
-	"Shoujo: 228",
-	"Shoujo Ai: 1403",
-	"Shounen: 246",
-	"Shounen Ai: 718",
-	"Slice of Life: 157",
-	"Smut: 736",
-	"Sports: 966",
-	"Supernatural: 995",
-	"Tragedy: 985",
-	"Xianxia: 245",
-	"Xuanhuan: 428",
-	"Yaoi: 184",
-	"Yuri: 182",
+    "Action: 111",
+    "Adult: 183",
+    "Adventure: 112",
+    "BL: 207",
+    "Comedy: 153",
+    "Drama: 115",
+    "Ecchi: 170",
+    "Fantasy: 114",
+    "Harem: 956",
+    "Historical: 178",
+    "Horror: 254",
+    "Josei: 472",
+    "Martial Arts: 1329",
+    "Mature: 427",
+    "Mecha: 1481",
+    "Mystery: 645",
+    "Psychological: 515",
+    "Reincarnation: 1031",
+    "Romance: 108",
+    "School Life: 545",
+    "Sci-Fi: 113",
+    "Seinen: 708",
+    "Shoujo: 228",
+    "Shoujo Ai: 1403",
+    "Shounen: 246",
+    "Shounen Ai: 718",
+    "Slice of Life: 157",
+    "Smut: 736",
+    "Sports: 966",
+    "Supernatural: 995",
+    "Tragedy: 985",
+    "Xianxia: 245",
+    "Xuanhuan: 428",
+    "Yaoi: 184",
+    "Yuri: 182",
 }
 
 
 local searchFilters = {
-	DropdownFilter(ORDER_BY_FILTER, "Order by", ORDER_BY_VALUES),
-	DropdownFilter(STATUS_FILTER, "Status", STATUS_VALUES),
-	FilterGroup("Genre", map(GENRE_VALUES, function(v, i) 
-		local KEY_ID = GENRE_FILTER + i
-		local key, _ = v:match("^(.-):%s*(.-)$")
-		return TriStateFilter(KEY_ID, key)
-	end))
+    DropdownFilter(ORDER_BY_FILTER, "Order by", ORDER_BY_VALUES),
+    DropdownFilter(STATUS_FILTER, "Status", STATUS_VALUES),
+    FilterGroup("Genre", map(GENRE_VALUES, function(v, i) 
+        local KEY_ID = GENRE_FILTER + i
+        local key, _ = v:match("^(.-):%s*(.-)$")
+        return TriStateFilter(KEY_ID, key)
+    end))
 }
 
 local function shrinkURL(url)
-	return url:gsub("^.-storyseedling%.com", "")
+    return url:gsub("^.-storyseedling%.com", "")
 end
 
 local function expandURL(url)
-	return baseURL .. url
+    return baseURL .. url
 end
 
 --- @param seriesUrl string
 local function rewriteSeriesUrl(seriesUrl)
-	-- rewrite from /novel/12345/series-slug
-	-- into: /series/12345
-	return seriesUrl:gsub("/novel/(%d+)/.*", "/series/%1")
+    -- rewrite from /novel/12345/series-slug
+    -- into: /series/12345
+    return seriesUrl:gsub("/novel/(%d+)/.*", "/series/%1")
 end
 
 --- Rewrite old chapter URLs to new ones
 --- @param chapterUrl string
 local function rewriteChapterUrl(chapterUrl)
-	-- rewrite the following variants:
-	-- - /novel/12345/series-slug/chapter-X => /series/12345/X
-	-- - /novel/12345/series-slug/chapter-X-Z => /series/12345/X.Z
-	-- - /novel/12345/series-slug/chapter-X (extra data) => /series/12345/X (extra data)
-	-- - /novel/12345/series-slug/chapter-X-Z (extra data) => /series/12345/X.Z (extra data)
-	-- - /novel/12345/series-slug/volume-X-chapter-Y => /series/12345/vX/Y
-	-- - /novel/12345/series-slug/volume-X-chapter-Y-Z => /series/12345/vX/Y.Z
+    -- rewrite the following variants:
+    -- - /novel/12345/series-slug/chapter-X => /series/12345/X
+    -- - /novel/12345/series-slug/chapter-X-Z => /series/12345/X.Z
+    -- - /novel/12345/series-slug/chapter-X (extra data) => /series/12345/X (extra data)
+    -- - /novel/12345/series-slug/chapter-X-Z (extra data) => /series/12345/X.Z (extra data)
+    -- - /novel/12345/series-slug/volume-X-chapter-Y => /series/12345/vX/Y
+    -- - /novel/12345/series-slug/volume-X-chapter-Y-Z => /series/12345/vX/Y.Z
 
-	local matches = {
-		chapterUrl:match("/novel/(%d+)/([^/]+)/chapter-(%d+)([^/]*)$"),
-		chapterUrl:match("/novel/(%d+)/([^/]+)/volume-(%d+)-chapter-(%d+)([^/]*)$")
-	}
+    local matches = {
+        chapterUrl:match("/novel/(%d+)/([^/]+)/chapter-(%d+)([^/]*)$"),
+        chapterUrl:match("/novel/(%d+)/([^/]+)/volume-(%d+)-chapter-(%d+)([^/]*)$")
+    }
 
-	if matches[1] then
-		local novelId = matches[1]
-		local chapterNum = matches[3]
-		local extraData = matches[4] or ""
-		return "/series/" .. novelId .. "/" .. chapterNum .. extraData
-	elseif matches[4] then
-		local novelId = matches[2]
-		local volume = "v" .. matches[4]
-		local chapterNum = matches[5]
-		local extraData = matches[6] or ""
-		return "/series/" .. novelId .. "/" .. volume .. "/" .. chapterNum .. extraData
-	else
-		-- Return original URL if no matches found
-		return chapterUrl
-	end   
+    if matches[1] then
+        local novelId = matches[1]
+        local chapterNum = matches[3]
+        local extraData = matches[4] or ""
+        return "/series/" .. novelId .. "/" .. chapterNum .. extraData
+    elseif matches[4] then
+        local novelId = matches[2]
+        local volume = "v" .. matches[4]
+        local chapterNum = matches[5]
+        local extraData = matches[6] or ""
+        return "/series/" .. novelId .. "/" .. volume .. "/" .. chapterNum .. extraData
+    else
+        -- Return original URL if no matches found
+        return chapterUrl
+    end   
 end
 
 local function getPassage(chapterURL)
-	local chap = GETDocument(expandURL(rewriteChapterUrl(chapterURL))):selectFirst("main")
+    local chap = GETDocument(expandURL(rewriteChapterUrl(chapterURL))):selectFirst("main")
 
-	local proseData = chap:selectFirst(".prose")
-	-- Remove empty <p> tags
-	local toRemove = {}
-	proseData:traverse(NodeVisitor(function(v)
-		if v:tagName() == "p" and v:text() == "" then
-			toRemove[#toRemove+1] = v
-		end
-		if v:hasAttr("border") then
-			v:removeAttr("border")
-		end
-	end, nil, true))
-	for _,v in pairs(toRemove) do
-		v:remove()
-	end
-	local notProse = proseData:selectFirst("div.not-prose")
-	if notProse ~= nil then
-		notProse:remove()
-	end
-	return pageOfElem(proseData, true)
+    local proseData = chap:selectFirst(".prose")
+    -- Remove empty <p> tags
+    local toRemove = {}
+    proseData:traverse(NodeVisitor(function(v)
+        if v:tagName() == "p" and v:text() == "" then
+            toRemove[#toRemove+1] = v
+        end
+        if v:hasAttr("border") then
+            v:removeAttr("border")
+        end
+    end, nil, true))
+    for _,v in pairs(toRemove) do
+        v:remove()
+    end
+    local notProse = proseData:selectFirst("div.not-prose")
+    if notProse ~= nil then
+        notProse:remove()
+    end
+    return pageOfElem(proseData, true)
 end
 
 --- @param description Element
 local function formatDescription(description)
-	local synopsis = ""
-	local totalNodes = description:childNodeSize()
-	for i = 0, totalNodes - 1 do
-		local node = description:childNode(i)
-		local textData = node:text():gsub("^%s*(.-)%s*$", "%1")
-		synopsis = synopsis .. textData .. "\n"
-	end
-	return synopsis:gsub("\n+$", ""):gsub("%s+$", "")
+    local synopsis = ""
+    local totalNodes = description:childNodeSize()
+    for i = 0, totalNodes - 1 do
+        local node = description:childNode(i)
+        local textData = node:text():gsub("^%s*(.-)%s*$", "%1")
+        synopsis = synopsis .. textData .. "\n"
+    end
+    return synopsis:gsub("\n+$", ""):gsub("%s+$", "")
 end
 
 local function parseNovel(novelURL, loadChapters)
-	local doc = GETDocument(expandURL(rewriteSeriesUrl(novelURL)))
-	local content = doc:selectFirst("main")
+    local doc = GETDocument(expandURL(rewriteSeriesUrl(novelURL)))
+    local content = doc:selectFirst("main")
 
-	local chapterSelector = content:selectFirst("section[x-data]")
-	local gridInInfo = chapterSelector:selectFirst(".lg\\:grid-in-info")
-	local gridInContent = chapterSelector:selectFirst(".lg\\:grid-in-content")
-	
-	local info = NovelInfo {
-		title = content:selectFirst("h1.text-2xl"):text(),
-		imageURL = content:selectFirst("div.bg-blur"):selectFirst("img"):attr("src"),
-		description = formatDescription(gridInContent:selectFirst(".order-2")),
-		artists = { "Translator: Story Seedling" },
-		genres = map(gridInInfo:select("a[up-deprecated]"), function(v) return v:text() end),
-	}
+    local chapterSelector = content:selectFirst("section[x-data]")
+    local gridInInfo = chapterSelector:selectFirst(".lg\\:grid-in-info")
+    local gridInContent = chapterSelector:selectFirst(".lg\\:grid-in-content")
+    
+    local info = NovelInfo {
+        title = content:selectFirst("h1.text-2xl"):text(),
+        imageURL = content:selectFirst("div.bg-blur"):selectFirst("img"):attr("src"),
+        description = formatDescription(gridInContent:selectFirst(".order-2")),
+        artists = { "Translator: Story Seedling" },
+        genres = map(gridInInfo:select("a[up-deprecated]"), function(v) return v:text() end),
+    }
 
-	if loadChapters then
-		local baseSelector = chapterSelector:selectFirst('div[x-show.transition.in.opacity.duration.600]')
-		local selectChapters = baseSelector:select("a[up-deprecated]")
-		local chapterSize = selectChapters:size()
-		-- Start from last chapter to first since website shows it in reverse order
-		-- This is to keep the order consistent with other sources
-		local _chapters = {}
-		for i = chapterSize - 1, 0, -1 do
-			local v = selectChapters:get(i)
-			-- This is to ignore the premium chapter, those have a lock icon in their anchor.
-			local firstPremChapter = v:selectFirst("svg.feather-droplet")
-			if firstPremChapter == nil then
-				local divBase = v:selectFirst("div")
-				_chapters[#_chapters + 1] = NovelChapter {
-					order = #_chapters,
-					title = divBase:selectFirst(".truncate"):text(),
-					link = shrinkURL(v:attr("href")),
-					release = divBase:selectFirst("small.text-xs"):text()
-				}
-			end
-		end
+    if loadChapters then
+        local baseSelector = chapterSelector:selectFirst('div[x-show.transition.in.opacity.duration.600]')
+        local selectChapters = baseSelector:select("a[up-deprecated]")
+        local chapterSize = selectChapters:size()
+        -- Start from last chapter to first since website shows it in reverse order
+        -- This is to keep the order consistent with other sources
+        local _chapters = {}
+        for i = chapterSize - 1, 0, -1 do
+            local v = selectChapters:get(i)
+            -- This is to ignore the premium chapter, those have a lock icon in their anchor.
+            local firstPremChapter = v:selectFirst("svg.feather-droplet")
+            if firstPremChapter == nil then
+                local divBase = v:selectFirst("div")
+                _chapters[#_chapters + 1] = NovelChapter {
+                    order = #_chapters,
+                    title = divBase:selectFirst(".truncate"):text(),
+                    link = shrinkURL(v:attr("href")),
+                    release = divBase:selectFirst("small.text-xs"):text()
+                }
+            end
+        end
 
-		info:setChapters(AsList(_chapters))
-	end
-	return info
+        info:setChapters(AsList(_chapters))
+    end
+    return info
 end
 
 --- @param listing table
 local function parseListing(listing)
-	return map(listing.data.posts, function(v)
-		return Novel {
-			title = v.title,
-			link = shrinkURL(v.permalink),
-			imageURL = v.bigThumbnail,
-		}
-	end)
+    return map(listing.data.posts, function(v)
+        return Novel {
+            title = v.title,
+            link = shrinkURL(v.permalink),
+            imageURL = v.bigThumbnail,
+        }
+    end)
 end
 
 local function getPostData()
-	if globalState.postData then
-		return globalState.postData
-	end
+    if globalState.postData then
+        return globalState.postData
+    end
 
-	local webpage = GETDocument(expandURL("/browse/"))
+    local webpage = GETDocument(expandURL("/browse/"))
 
-	local axLoad = webpage:selectFirst("div[ax-load]")
-	local xData = axLoad:attr("x-data")
-	-- browse('RANDOMDATAHERE'), get the random data
-	local randomData = xData:match("browse%('([^']+)'%)")
+    local axLoad = webpage:selectFirst("div[ax-load]")
+    local xData = axLoad:attr("x-data")
+    -- browse('RANDOMDATAHERE'), get the random data
+    local randomData = xData:match("browse%('([^']+)'%)")
 
-	globalState.postData = randomData
+    globalState.postData = randomData
 
-	return randomData
+    return randomData
 end
 
 local function getSearch(data)
-	local query = data[QUERY]
-	local page = data[PAGE]
-	local orderBy = data[ORDER_BY_FILTER]
+    local query = data[QUERY]
+    local page = data[PAGE]
+    local orderBy = data[ORDER_BY_FILTER]
 
-	-- get the random data
-	local randomData = getPostData()
-	print("StorySeedling Random Data:", randomData)
+    -- get the random data
+    local randomData = getPostData()
+    print("StorySeedling Random Data:", randomData)
 
-	-- build form
-	local formBuilder = Multipartd:new()
-	formBuilder:add("search", query or "")
+    -- build form
+    local formBuilder = Multipartd:new()
+    formBuilder:add("search", query or "")
 
-	map(GENRE_VALUES, function(v, i)
-		local KEY_ID = GENRE_FILTER + i
+    map(GENRE_VALUES, function(v, i)
+        local KEY_ID = GENRE_FILTER + i
         local _, value = v:match("^(.-):%s*(.-)$")
-		if data[KEY_ID] == 1 then
-			formBuilder:add("includeGenres[]", value)
-		elseif data[KEY_ID] == 2 then
-			formBuilder:add("excludeGenres[]", value)
-		end
-	end)
+        if data[KEY_ID] == 1 then
+            formBuilder:add("includeGenres[]", value)
+        elseif data[KEY_ID] == 2 then
+            formBuilder:add("excludeGenres[]", value)
+        end
+    end)
 
-	if orderBy ~= nil then
-		formBuilder:add("orderBy", ORDER_BY_TERMS[orderBy + 1])
-	else
-		formBuilder:add("orderBy", "recent")
-	end
+    if orderBy ~= nil then
+        formBuilder:add("orderBy", ORDER_BY_TERMS[orderBy + 1])
+    else
+        formBuilder:add("orderBy", "recent")
+    end
 
-	if page > 1 then
-		formBuilder:add("curpage", tostring(page))
-	end
+    if page > 1 then
+        formBuilder:add("curpage", tostring(page))
+    end
 
-	formBuilder:add("post", randomData)
-	formBuilder:add("action", "fetch_browse")
+    formBuilder:add("post", randomData)
+    formBuilder:add("action", "fetch_browse")
 
-	-- for media type, cut off the first two dashes
-	local headers = HeadersBuilder()
-	headers:add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0")
-	headers:add("Origin", "https://storyseedling.com")
-	headers:add("Referer", "https://storyseedling.com/browse")
+    -- for media type, cut off the first two dashes
+    local headers = HeadersBuilder()
+    headers:add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0")
+    headers:add("Origin", "https://storyseedling.com")
+    headers:add("Referer", "https://storyseedling.com/browse")
 
-	local resp = Request(POST(
-		expandURL("/ajax"),
-		headers:build(),
-		RequestBody(formBuilder:build(), MediaType(formBuilder:getHeader()))
-	))
+    local resp = Request(POST(
+        expandURL("/ajax"),
+        headers:build(),
+        RequestBody(formBuilder:build(), MediaType(formBuilder:getHeader()))
+    ))
 
-	-- json response
-	local body = resp:body():string()
-	local jsonData = json.decode(body)
-	return parseListing(jsonData)
+    -- json response
+    local body = resp:body():string()
+    local jsonData = json.decode(body)
+    return parseListing(jsonData)
 end
 
 local function getListing(data)
-	return getSearch(data)
+    return getSearch(data)
 end
 
 return {
-	id = 4302,
-	name = "Story Seedling",
-	baseURL = baseURL,
-	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/TravisTranslations.png",
-	chapterType = ChapterType.HTML,
+    id = 4302,
+    name = "Story Seedling",
+    baseURL = baseURL,
+    imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/TravisTranslations.png",
+    chapterType = ChapterType.HTML,
 
-	listings = {
-		Listing("Latest", true, getListing)
-	},
-	getPassage = getPassage,
-	parseNovel = parseNovel,
+    listings = {
+        Listing("Latest", true, getListing)
+    },
+    getPassage = getPassage,
+    parseNovel = parseNovel,
 
-	hasSearch = true,
-	isSearchIncrementing = true,
-	search = getSearch,
-	searchFilters = searchFilters,
+    hasSearch = true,
+    isSearchIncrementing = true,
+    search = getSearch,
+    searchFilters = searchFilters,
 
-	shrinkURL = shrinkURL,
-	expandURL = expandURL
+    shrinkURL = shrinkURL,
+    expandURL = expandURL
 }
